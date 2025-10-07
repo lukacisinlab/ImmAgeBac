db7dc95104216bd4fcf85fdefbbd9db2e41d4755
Hruzko <hruzko@gmail.com>
2025-10-07 14:56:51 +0200

    Graph work

--- figures/fig_one/pearson_bar_chart/bar.py
+++ figures/fig_one/pearson_bar_chart/bar.py
@@ -7,12 +7,25 @@ NUMBER_OF_PATHWAYS = 7
 
 gsea_results = pd.read_csv("gsea_results_pearson_gbp_v_0.2.csv")
 gsea_results = gsea_results.sort_values('NES')
-
-plt.figure(figsize=(14, 10))  
-barplot = sns.barplot(x='NES', y='Term', data = pd.concat([gsea_results.head(NUMBER_OF_PATHWAYS), gsea_results.tail(NUMBER_OF_PATHWAYS)]), color="black")
-
+print(gsea_results.head(7)[['NES', 'FDR q-val']])
+print(gsea_results.tail(7)[['NES', 'FDR q-val']])
 
 
+plt.figure(figsize=(14, 10)) 
+subset = pd.concat([gsea_results.head(NUMBER_OF_PATHWAYS), gsea_results.tail(NUMBER_OF_PATHWAYS)]) 
+print(subset[['NES', 'FDR q-val']])
+ax = barplot = sns.barplot(x='NES', y='Term', data = subset, color="black")
+
+for i, (value, fdr) in enumerate(zip(subset['NES'], subset['FDR q-val'])):
+    ax.text(
+        x=value + 0.1 if value > 0 else value - 0.1,
+        y=i,
+        s=f"FDR={fdr:.5f}",
+        va='center',
+        ha='left' if value > 0 else 'right',
+        color='black',
+        fontsize=8,
+    )
 
 plt.xlabel('Normalized Enrichment Score (NES)')
 plt.ylabel('Pathway')
